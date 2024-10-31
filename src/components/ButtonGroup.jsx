9b9d012dbf868a60e1644b2796f083de9d821e78
Luke Armstrong <Ltarmstrong96@gmail.com>
2024-10-31 09:40:17 -0700

    first commit

---
+++ src/components/ButtonGroup.jsx
@@ -0,0 +1,43 @@
+// import { secondaryButtons } from "../lib/constants";
+import Button from "./Button";
+
+export default function ButtonGroup({
+  handleRemoveAllItems,
+  handleResetToInitial,
+  handleMarkAllAsComplete,
+  handleMarkAllAsIncomplete,
+}) {
+  const secondaryButtons = [
+    {
+      text: "Mark all as complete",
+      onClick: handleMarkAllAsComplete,
+    },
+    {
+      text: "Mark all as incomplete",
+      onClick: handleMarkAllAsIncomplete,
+    },
+    {
+      text: "Reset to initial",
+      onClick: handleResetToInitial,
+    },
+    {
+      text: "Remove all items",
+      onClick: handleRemoveAllItems,
+    },
+  ];
+  return (
+    <section className="button-group">
+      {secondaryButtons.map(({ text, onClick }) => {
+        return (
+          <Button
+            key={text + onClick.toString()}
+            onClick={onClick}
+            type="secondary"
+          >
+            {text}
+          </Button>
+        );
+      })}
+    </section>
+  );
+}
