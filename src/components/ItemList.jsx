9555a5e5d807dfac7a8ecaa83216747271508473
Luke Armstrong <Ltarmstrong96@gmail.com>
2024-11-04 09:17:17 -0800

    Replacing context API with Zustand for better computing management

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,7 +1,7 @@
 import Select from "react-select";
 import EmptyView from "./EmptyView";
 import { useMemo, useState } from "react";
-import { useItemsContext } from "../lib/hooks";
+import { useItemsStore } from "../stores/itemsStore";
 
 const sortingOptions = [
   {
@@ -20,7 +20,9 @@ const sortingOptions = [
 
 export default function ItemList() {
   const [sortBy, setSortBy] = useState("default");
-  const { items, handleDeleteItem, handleToggleItem } = useItemsContext();
+  const deleteItem = useItemsStore(state => state.deleteItem);
+  const toggleItem = useItemsStore(state => state.toggleItem);
+  const items = useItemsStore(state => state.items);
 
   const sortedItems = useMemo(
     () =>
@@ -57,8 +59,8 @@ export default function ItemList() {
           <Item
             key={item.id}
             item={item}
-            onDeleteItem={handleDeleteItem}
-            onToggleItem={handleToggleItem}
+            onDeleteItem={deleteItem}
+            onToggleItem={toggleItem}
           />
         );
       })}
